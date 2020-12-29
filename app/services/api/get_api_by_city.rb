3d642d28dd2828591888df7360c375f165503eb2
Jorge Gonzalez <jorg3.594@gmail.com>
2020-12-29 14:20:09 -0500

    fix search by city

--- app/services/api/get_api_by_city.rb
+++ app/services/api/get_api_by_city.rb
@@ -9,13 +9,14 @@ class Api::GetApiByCity
   private
 
   def get_data(city)
+    uri = "https://api.turismoi.com/api/tours?page%5Bpage%5D=1&page%5Bper_page%5D=10&filter%5Bcities%5D=#{city}"
     headers = {
       'Content-Type': 'application/vnd.api+json',
       'accept': 'application/vnd.api+json',
+      'Accept-Search-Filters': 'yes',
       'Authorization': 'Token eyJhbGciOiJIUzI1NiJ9.eyJyZXNlbGxlcl9lbWFpbCI6ImFwYWRpbGxhK3BydWViYWRldkB0dXJpc21vaS5jb20ifQ.ICmqJen12eyoyNfKlMoSkZG5yffULVVNBalbqztFxoU'
     }
-    response = HTTP.headers(headers)
-                   .get('https://api.turismoi.com/api/tours?page%5Bpage%5D=1&page%5Bper_page%5D=10')
+    response = HTTP.headers(headers).get(uri)
     JSON.parse(response.to_s)
   end
 
@@ -24,24 +25,24 @@ class Api::GetApiByCity
       activities[activity['id']] = activity['attributes']
     end
 
-    data["data"].map do |tour|
-      activities = tour["relationships"]["activities"]["data"].map { |activity| activities_data[activity["id"]] }
-      tour["activities"] = activities
+    data['data'].map do |tour|
+      activities = tour['relationships']['activities']['data'].map { |activity| activities_data[activity['id']] }
+      tour['activities'] = activities
       tour
     end
   end
 
   def create_hash_by_tour(data)
     data.map do |tour|
-      new_hash = {}
       avg_hours = tour['attributes']['avg_hours']
-      new_hash[:name] = tour['attributes']['name']
-      new_hash[:activities] = tour['activities'].map { |activity| activity['name'] }
-      new_hash[:hours] = avg_hours != 24 ? avg_hours : null
-      new_hash[:days] = tour['attributes']['days']
-      new_hash[:price] = tour['attributes']['original_price']
-      new_hash[:photo] = tour['attributes']['principal_photo']
-      new_hash
+      {
+        name: tour['attributes']['name'],
+        activities: tour['activities'].map { |activity| activity['name'] },
+        hours: avg_hours != 24 ? avg_hours : nil,
+        days: tour['attributes']['days'],
+        price: tour['attributes']['original_price'],
+        photo: tour['attributes']['principal_photo']
+      }
     end
   end
 end
